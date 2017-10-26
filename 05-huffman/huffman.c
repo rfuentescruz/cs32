f844ade10137b86b520e1646158915985c93ca1b
Rolando Cruz <rfuentescruz@users.noreply.github.com>
2017-10-26 22:56:55 +0800

    [mp5] Working version
    Implemented timestamping to correct the priority of elements and
    made use of integer frequency counts directly to avoid floating
    point precision problems.

--- 05-huffman/huffman.c
+++ 05-huffman/huffman.c
@@ -9,15 +9,17 @@ typedef struct Node {
     int freq;
     struct Node *left;
     struct Node *right;
+    int ts;
 } Node;
 
 typedef struct PriorityQueue {
     Node *heap[PQ_HEAP_MAX_SIZE];
     int size;
+    int ts;
 } PriorityQueue;
 
 typedef struct PrefixStack {
-    char stack[52];
+    char stack[PQ_HEAP_MAX_SIZE];
     int head;
 } PrefixStack;
 
@@ -29,18 +31,24 @@ void pq_insert(PriorityQueue *pq, Node *);
 Node *pq_extract(PriorityQueue *pq);
 void pq_destroy(PriorityQueue *pq);
 
-void preorder_prefixes(Node *root, PrefixStack *prefixes);
+void build_prefixes(Node *root, PrefixStack *prefixes, char *map[]);
 void heapify(PriorityQueue *pq, int root);
 void heapsort(PriorityQueue *pq);
+int check_order(Node *a, Node *b);
 
 int main(int argc, char *argv[]) {
     int char_count = 0;
     int map[256];
+    char *prefix_map[256];
 
     PriorityQueue *pq = pq_init();
 
     for (int i = 0; i < 256; i++) {
         map[i] = 0;
+    }
+
+    for (int i = 0; i < 256; i++) {
+        prefix_map[i] = NULL;
     }
 
     char c = '\0';
@@ -51,24 +59,25 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    float freq = 0.0;
     for (int i = 0; i < 256; i++) {
         if (isalpha((char) i)) {
-            freq = (float) map[i] / char_count;
-            freq *= 10000; // 100.00
             if (map[i]) {
-                printf("%c: %d\n", (char) i, map[i]);
-                Node *n = new_node((char) i, (int) freq);
+                #if DEBUG
+                    printf("%c: %d\n", (char) i, map[i]);
+                #endif
+                Node *n = new_node((char) i, map[i]);
                 pq_insert(pq, n);
             }
         }
     }
-
+    
     while (pq->size > 1) {
         Node *left = pq_extract(pq);
-        printf("Left: (%c) %.2f\n", left->c, left->freq / 100.0);
         Node *right = pq_extract(pq);
-        printf("Right: (%c) %.2f\n", right->c, right->freq / 100.0);
+        #if DEBUG
+            printf("Left: (%c) %.2f\n", left->c, left->freq);
+            printf("Right: (%c) %.2f\n", right->c, right->freq);
+        #endif
         Node *n = new_node('\0', left->freq + right->freq);
         n->left = left;
         n->right = right;
@@ -82,10 +91,17 @@ int main(int argc, char *argv[]) {
     }
     prefixes->head = -1;
 
-    preorder_prefixes(n, prefixes);
+    build_prefixes(n, prefixes, prefix_map);
     free(prefixes);
     destroy_node(n);
     pq_destroy(pq);
+
+    for (int i = 0; i < 256; i++) {
+        if (prefix_map[i] != NULL) {
+            printf("%c %s\n", (char) i, prefix_map[i]);
+            free(prefix_map[i]);
+        }
+    }
 }
 
 Node *new_node(char c, int freq) {
@@ -94,12 +110,14 @@ Node *new_node(char c, int freq) {
     n->freq = freq;
     n->left = NULL;
     n->right = NULL;
+    n->ts = 0;
     return n;
 }
 
 PriorityQueue *pq_init() {
     PriorityQueue *pq = malloc(sizeof(PriorityQueue));
     pq->size = 0;
+    pq->ts = 0;
     for (int i = 0; i < PQ_HEAP_MAX_SIZE; i++) {
         pq->heap[i] = NULL;
     }
@@ -111,8 +129,10 @@ void pq_insert(PriorityQueue *pq, Node *n) {
     if (pq->size == PQ_HEAP_MAX_SIZE) {
         return;
     }
+    n->ts = pq->ts;
     pq->heap[pq->size] = n;
     pq->size++;
+    pq->ts++;
     heapsort(pq);
 }
 
@@ -149,15 +169,19 @@ void destroy_node(Node *n) {
     free(n);
 }
 
-void preorder_prefixes(Node *root, PrefixStack *prefixes) {
+void build_prefixes(Node *root, PrefixStack *prefixes, char *map[]) {
     if (root->c != '\0') {
-        printf("%c: %s\n", root->c, prefixes->stack);
+        int len = strlen(prefixes->stack);
+        if (len) {
+            map[(int) root->c] = (char *) malloc(len + 1);
+            strcpy(map[(int) root->c], prefixes->stack);
+        }
     }
 
     if (root->left != NULL) {
         prefixes->head++;
         prefixes->stack[prefixes->head] = '0';
-        preorder_prefixes(root->left, prefixes);
+        build_prefixes(root->left, prefixes, map);
         prefixes->stack[prefixes->head] = '\0';
         prefixes->head--;
     }
@@ -165,7 +189,7 @@ void preorder_prefixes(Node *root, PrefixStack *prefixes) {
     if (root->right != NULL) {
         prefixes->head++;
         prefixes->stack[prefixes->head] = '1';
-        preorder_prefixes(root->right, prefixes);
+        build_prefixes(root->right, prefixes, map);
         prefixes->stack[prefixes->head] = '\0';
         prefixes->head--;
     }
@@ -175,13 +199,22 @@ void heapify(PriorityQueue *pq, int root) {
     Node *root_node = pq->heap[root];
     int child = (root * 2) + 1;
 
+    Node *child_node = NULL,
+         *sibling = NULL;
     while (child < pq->size) {
-        if (child + 1 < pq->size && pq->heap[child + 1]->freq < pq->heap[child]->freq) {
-            child += 1;
+        child_node = pq->heap[child];
+        if (child + 1 < pq->size) {
+            sibling = pq->heap[child + 1];
+            if (check_order(child_node, sibling) == 1) {
+                child_node = sibling;
+                child += 1;
+            }
+        } else {
+            sibling = NULL;
         }
 
-        if (pq->heap[child]->freq < root_node->freq) {
-            pq->heap[root] = pq->heap[child];
+        if (check_order(root_node, child_node) == 1) {
+            pq->heap[root] = child_node;
             root = child;
             child = (root * 2) + 1;
         } else {
@@ -195,4 +228,24 @@ void heapsort(PriorityQueue *pq) {
     for (int i = (pq->size - 1) / 2; i >= 0; --i) {
         heapify(pq, i);
     }
+}
+
+/**
+ * check_order(Node *a, Node *b)
+ * 
+ * Compare two nodes and determine which should be a root node when in an
+ * almost heap
+ * 
+ * Returns 1 if b should be root and -1 if a should be root
+ */
+int check_order(Node *a, Node *b) {
+    if (b->freq < a->freq) {
+        return 1;
+    }
+
+    if (b->freq == a->freq && b->ts < a->ts) {
+        return 1;
+    }
+
+    return -1;
 }
